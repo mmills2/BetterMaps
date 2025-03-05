77b601e50a98c3555a82c26dbf67ba14791aeaae
mmills2 <30macm@gmail.com>
2025-03-05 17:12:39 -0500

    google maps api basics

--- app/page.js
+++ app/page.js
@@ -1,3 +1,50 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+"use client";
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "700px",
+  height: "700px",
+};
+
+const center = {
+  lat: 40.747839,
+  lng: -73.990965,
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API,
+  });
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={13}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
+
+export default React.memo(MyComponent);
